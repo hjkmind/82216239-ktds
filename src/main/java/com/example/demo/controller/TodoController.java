8c47528ea2a656d34d9b8e9b80187c202871bef8
hjkmind <hjkmind@naver.com>
2024-11-27 16:24:41 +0900

    code update

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,13 +1,13 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TodoController {
 
-    @GetMapping("/")
-    public String home() {
-        return "todoList";
+    @GetMapping("/api/v1/user")
+    public String getUser() {
+        return "82216239";
     }
 }
